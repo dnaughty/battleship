a86648305c3c8bac4511817275045de405a633a4
Denis Naughton <mjolinor75@hotmail.com>
2020-08-18 18:59:01 -0400

    added initialize

--- lib/board.rb
+++ lib/board.rb
@@ -1,7 +1,13 @@
 class Board
 
-    def initialize(n)
+    attr_reader :size
 
+    def initialize(n)
+        @grid = Array.new(n) { Array.new(n,:N) }
+        @size = n * n
+    end
+
+    def [](arr)
 
     end
 
